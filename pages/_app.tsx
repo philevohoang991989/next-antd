e9506e0b4555956c99799b4cd1696b1c96881386
HoangPhi <phi.le@bnksolution.com>
2023-03-29 13:42:43 +0700

    add vscode setting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,40 +1,40 @@
-import axiosClient from "@/api-client/axios-client";
-import { EmptyLayout } from "@/components/layout";
-import { AppPropsWithLayout } from "@/models";
-import "@/styles/globals.css";
-import { createEmotionCache } from "@/utils";
-import { CacheProvider } from "@emotion/react";
-import { Spin } from 'antd';
-import "antd/dist/reset.css";
-import { useEffect, useState } from "react";
-import { SWRConfig } from "swr";
+import axiosClient from '@/api-client/axios-client'
+import { EmptyLayout } from '@/components/layout'
+import { AppPropsWithLayout } from '@/models'
+import '@/styles/globals.css'
+import { createEmotionCache } from '@/utils'
+import { CacheProvider } from '@emotion/react'
+import { Spin } from 'antd'
+import 'antd/dist/reset.css'
+import { useEffect, useState } from 'react'
+import { SWRConfig } from 'swr'
 
 // Client-side cache, shared for the whole session of the user in the browser.
-const clientSideEmotionCache = createEmotionCache();
+const clientSideEmotionCache = createEmotionCache()
 
 export default function App({
   Component,
   pageProps,
-  emotionCache = clientSideEmotionCache,
+  emotionCache = clientSideEmotionCache
 }: AppPropsWithLayout) {
-  const Layout = Component.Layout ?? EmptyLayout;
-  const [showChild, setShowChild] = useState(false);
+  const Layout = Component.Layout ?? EmptyLayout
+  const [showChild, setShowChild] = useState(false)
   useEffect(() => {
-    setShowChild(true);
-  }, []);
+    setShowChild(true)
+  }, [])
 
   if (!showChild) {
-    return null;
+    return null
   }
-  if (typeof window === "undefined") {
-    return <Spin/>;
+  if (typeof window === 'undefined') {
+    return <Spin />
   } else {
     return (
       <CacheProvider value={emotionCache}>
         <SWRConfig
           value={{
             fetcher: (url) => axiosClient.get(url),
-            shouldRetryOnError: false,
+            shouldRetryOnError: false
           }}
         >
           <Layout>
@@ -42,6 +42,6 @@ export default function App({
           </Layout>
         </SWRConfig>
       </CacheProvider>
-    );
+    )
   }
 }
