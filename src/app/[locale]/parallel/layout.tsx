ffa12548fa824b282c5ea2f2c135cb6d308c94a5
Hoang Phi <phi.le@bnksolution.com>
2024-10-03 16:10:39 +0700

    update demo parallel

---
+++ src/app/[locale]/parallel/layout.tsx
@@ -0,0 +1,14 @@
+
+export default function ParallelLayout({
+    children,
+    team,
+    user,
+  }: {
+    children: React.ReactNode
+    team: React.ReactNode
+    user: React.ReactNode
+  }) {
+    return <div className="bg-[#e5f9ff]">{children} {team}
+    {user}</div>;
+  }
+  
